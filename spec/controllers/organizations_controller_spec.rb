b6639926b46a1dfe11b19cc476434968586b27b2
Evan Sparkman <me@evansparkman.com>
2019-03-07 16:19:02 -0500

    Finish out basic specs

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe OrganizationsController, type: :controller do
+def sign_in(user)
+  session[:user_id] = user.id
+end
 
+RSpec.describe OrganizationsController, type: :controller do
+  before do
+    @user = FactoryBot.create(:user)
+    @organization = FactoryBot.create(:organization)
+  end
+  describe "#index" do
+    it "responds successfully" do
+      sign_in @user
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe "#show" do
+    it "responds successfully" do
+      sign_in @user
+      get :show, params: { id: @organization.id }
+      expect(response).to be_successful
+    end
+  end
 end
