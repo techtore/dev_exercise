95d7955391dc2d7fde2f908426feeeedee51cc35
Victoria Hale <vic.hale74@gmail.com>
2020-09-03 16:40:23 -0400

    Search results in ascedning order

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,11 +6,12 @@ class SearchController < ApplicationController
             redirect_to root_path, alert: "Please enter a search term" 
         else
             #filter result to search name column and set column to lowecase before searching, match search to user's input
-            @organizations = Organization.all.where("lower(name) LIKE :search", search: "%#{params[:search]}")  
+            @organizations = Organization.all.where("lower(name) LIKE ? ", "%#{params[:search]}").order('name')  
+            @search_term = params[:search]
         end
     end
 
-    # def search_params
-    #     params.permit(:search_term)
-    # end
+    def search_params
+        params.permit(:search)
+    end
 end
