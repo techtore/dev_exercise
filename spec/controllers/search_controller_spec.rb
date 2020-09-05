d8bd713d399c45f5611c304a979cb237f639f566
Victoria Hale <vic.hale74@gmail.com>
2020-09-05 12:46:50 -0400

    add tests

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
-
+    before do
+        @organization = FactoryBot.create(:organization)
+      end
+      describe "#organizations" do
+        context "given a search query" do
+            it "returns organizations" do
+                expect(response).to be_successful
+            end
+        end
+      end
 end
